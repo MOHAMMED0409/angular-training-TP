7bb5b2f4a4191a2bcc487a75e7c2dcf44fd12a1d
mohammed khashif <mohammedkhashif02@gmail.com>
2025-02-18 09:28:27 +0530

    Javascript Started

---
+++ javascript-training/js/script.js
@@ -0,0 +1,8 @@
+function welmsg(){
+    let username = document.getElementById("name").value;
+    console.log(username);
+    alert('Hi '+username+', Welcome to Websites!');
+    document.write(<marquee>Welcome to Websites!</marquee>);
+    window.open("https://www.google.com");
+
+}
